b24322e98c7a909f2db35dd765464f990ebbfeb5
Dominyka <dominyka.juchneviciute@gmail.com>
2026-09-25 17:40:54 +0300

    Sutvarkyta endpointer'iai pagal destytojo siulyma

--- EShop.Api/Controllers/ItemsController.cs
+++ EShop.Api/Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Http;
+using EShop.Api.Models;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace EShop.Api.Controllers
 {
@@ -7,10 +9,80 @@ namespace EShop.Api.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
-        [HttpGet("status")]
-        public IActionResult Status()
+        private static readonly List<Item> items = new()
         {
-            return Ok("Sveiki! Backend serveris veikia sėkmingai.");
+            new Item(
+                "Miesto dviratis",
+                ItemCondition.Used,
+                1,
+                "Tvarkingas dviratis, paruoštas sezonui.",
+                26,
+                "Kross",
+                "Juoda"
+            ),
+
+            new Item(
+                "Dell 24\" monitorius",
+                ItemCondition.Used,
+                2,
+                "Full HD monitorius be defektų.",
+                24,
+                "Dell",
+                "Juoda"
+            ),
+
+            new Item(
+                "C# Programavimo vadovėlis",
+                ItemCondition.New,
+                3,
+                "Naudinga knyga .NET programuotojams.",
+                null,
+                "Alma littera",
+                "Mėlyna"
+            )
+        };
+
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            return Ok(items);
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = items.FirstOrDefault(i => i.Id == id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateItem(Item item)
+        {
+            items.Add(item);
+
+            return Created($"/api/items/{item.Id}", item);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteItem(int id)
+        {
+            var item = items.FirstOrDefault(i => i.Id == id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            items.Remove(item);
+
+            return NoContent();
+        }
+
     }
 }
--- EShop.Api/Models/Item.cs
+++ EShop.Api/Models/Item.cs
@@ -4,15 +4,18 @@ public class Item
 {
     public string Name {get; set;}
     public ItemCondition Condition {get; set;}
+    public int Id { get; set; }
     public string? Description {get; set;}
     public int? Size {get; set;}
     public string? Manufacturer {get; set;}
     public string? Color {get; set;}
+    
 
-    public Item (string name, ItemCondition condition, string? description = null, int? size = null, string? manufacturer = null, string? color = null)
+    public Item (string name, ItemCondition condition,int id, string? description = null, int? size = null, string? manufacturer = null, string? color = null)
     {
         Name = name;
         Condition = condition;
+        Id= id;
         Description = description;
         Size = size;
         Manufacturer = manufacturer;
